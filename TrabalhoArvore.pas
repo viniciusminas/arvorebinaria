12c200de1b2491be2b544acacd1e57d8fb182fc2
Pedro Henrique Scheidt <132633289+HSP303@users.noreply.github.com>
2024-11-02 15:40:59 -0300

    Update TrabalhoArvore.pas

--- TrabalhoArvore.pas
+++ TrabalhoArvore.pas
@@ -1,12 +1,12 @@
-Program Trabalho_arv;                                                    
-type                                                          
+Program Trabalho_arv ;
+type
 		nodo_municipios = ^nodo_mun;
     nodo_mun = Record
         sigla_uf: string;
         desc_municipio: string;
-        mundir, munesq, munpai: nodo_municipios;
+        munesq, munesq, munpai: nodo_municipios;
     end;
-    
+        
     nodo_estados = ^nodo_est;
     nodo_est = Record
         uf: string;
@@ -21,7 +21,7 @@ Procedure EmOrdem(Raiz: nodo_estados);
 begin
     if Raiz <> nil then 
     begin
-        EmOrdem(Raiz^.estesq);
+        EmOrdem(Raiz^.estesq); 
         write(Raiz^.uf, '-');
         EmOrdem(Raiz^.estdir);
     end;
@@ -33,22 +33,11 @@ begin
 	//mun :=nil;	
 end;  
 
-Procedure ExibirMunicipios(RaizMunicipio: nodo_municipios);
-begin
-	if RaizMunicipio <> nil then
-	begin
-		write(RaizMunicipio^.desc_municipio, ', ');
-		ExibirMunicipios(RaizMunicipio^.munesq);
-		ExibirMunicipios(RaizMunicipio^.mundir);
-	end;
-end;
-
 Procedure PreOrdem(Raiz: nodo_estados);
 begin
     if Raiz <> nil then 
     begin
         write(Raiz^.uf, '-');
-        ExibirMunicipios(Raiz^.municipio); //teste para mostrar os mun's.
         PreOrdem(Raiz^.estesq);
         PreOrdem(Raiz^.estdir);
     end;
@@ -66,13 +55,14 @@ begin
   	
   	while achou = 0 do
   	begin
+  		aux := auxant^.estesq;
   		if auxant^.uf = estuf then
   		begin
   			VerificarEstadoExiste := auxant;
   			//writeln('Existe');
   			achou := 1;
   		end
-
+  		//Verifica Esquerda
   		else if estuf < auxant^.uf then
   		begin
       	if aux = nil then
@@ -103,19 +93,6 @@ begin
       	end
 			end	
   	end
-end;
-
-
-function VerificarMunicipioExiste(arvmun: nodo_municipios; municipio: string):boolean;
-begin
-	if arvmun = nil then
-		VerificarMunicipioExiste := false
-	else if arvmun^.desc_municipio = municipio then //se o nome inserido ja existe no nó
-		VerificarMunicipioExiste := true
-	else if municipio < arvmun^.desc_municipio then
-	  VerificarMunicipioExiste := VerificarMunicipioExiste(arvmun^.munesq, municipio)
-	else
-      VerificarMunicipioExiste := VerificarMunicipioExiste(arvmun^.mundir, municipio);
 end; 
 
 procedure CriarEstado(var arvraiz: nodo_estados; estuf: string);
@@ -156,125 +133,126 @@ begin
 		
 end;
 
-procedure CriarMunicipio(var arvmun: nodo_municipios; municipio: string);
-var novoMun: nodo_municipios;
+function AcharPosicao(var arvraiz: nodo_estados; estuf: string): nodo_estados;
 begin
-    new(novoMun);
-    novoMun^.desc_municipio := municipio;
-    novoMun^.munesq := nil;
-    novoMun^.mundir := nil;           
-    novoMun^.munpai := nil;
-
-    if arvmun = nil then
-        arvmun := novoMun
-    else
-    begin
-        if municipio < arvmun^.desc_municipio then
-            CriarMunicipio(arvmun^.munesq, municipio)
-        else
-            CriarMunicipio(arvmun^.mundir, municipio);
-    end;
+	if (arvraiz^.uf < estuf) then
+	begin 	
+		if (arvraiz^.estdir = nil) then
+			AcharPosicao := arvraiz
+		else
+			AcharPosicao(arvraiz^.estdir, estuf);	
+	end
+	else
+	begin
+		if (arvraiz^.estesq = nil) then
+			AcharPosicao := arvraiz
+		else
+			AcharPosicao(arvraiz^.estesq, estuf);	
+	end
+				
 end;
 
-{function PosicionarMunicipio();
+procedure ExcluirEstado(var arvraiz: nodo_estados;estuf: string);
+var estado, aux: nodo_estados;
+begin
+	if arvraiz^.uf = estuf then
+	begin
+		writeln('Raiz da Árvore');
+	end
+	else
+	begin
+		estado := VerificarEstadoExiste(arvraiz, estuf);
+		if (estado^.uf = estuf) then
+			begin
+				if (estado^.estesq = nil) and (estado^.estdir = nil) then
+				begin
+					if estado = estado^.estpai^.estesq then
+						estado^.estpai^.estesq := nil	
+					else
+					  estado^.estpai^.estdir := nil;
+					dispose(estado);
+				end
+				else
+				begin
+					if estado^.estpai^.uf < estado^.uf then
+					begin
+						if estado^.estesq <> nil then
+							estado^.estpai^.estdir := estado^.estesq
+						else
+							estado^.estpai^.estdir := estado^.estdir;
+						writeln(estado^.estpai^.estdir^.uf, ' ', estado^.estpai^.estesq^.uf);
+						aux := AcharPosicao(estado^.estesq, estado^.estdir^.uf);
+						aux^.estdir := estado^.estdir;
+						estado^.estdir^.estpai := aux;
+						estado^.estesq^.estdir := estado^.estdir;
+						dispose(estado);
+						//writeln('Novo Pai: ', AcharPosicao(estado^.estesq, estuf)^.uf);
+					end
+					else
+					begin
+						if estado^.estdir <> nil then 
+							estado^.estpai^.estesq := estado^.estdir
+						else
+							estado^.estpai^.estesq := estado^.estesq;
+							
+						writeln(estado^.estpai^.estdir^.uf, ' ', estado^.estpai^.estesq^.uf);
+						aux := AcharPosicao(estado^.estdir, estado^.estesq^.uf);
+						aux^.estesq := estado^.estesq;
+						estado^.estesq^.estpai := aux;
+						estado^.estdir^.estesq := estado^.estesq;
+						dispose(estado);
+						writeln(arvraiz^.estdir^.estesq^.uf, ' ', arvraiz^.estdir^.estdir^.uf);
+					end
+				end;	
+			end
+		else
+			writeln('Não da para excluir');	
+	end;
+end;
+		{
+function CriarMunicipio();
+begin
+end;
+
+function PosicionarMunicipio();
 begin
 end;
 
 function VerificarUltimoMunicipio();
 begin
-end;}
-
-procedure IncluirMunicipio(var arvraiz: nodo_estados; uf, municipio: string);
-var 
-		est: nodo_estados;
-begin
-    est := arvraiz;
-			
-    if VerificarEstadoExiste(arvraiz, uf)^.uf <> uf then
-        CriarEstado(arvraiz, uf);
-
-    while (est <> nil) and (est^.uf <> uf) do
-    begin
-        if uf < est^.uf then
-            est := est^.estesq
-        else
-            est := est^.estdir;
-    end;
-	
-	//condicional para adicionar o municipio na árvore desse estado
-	if (est <> nil) and (not VerificarMunicipioExiste(est^.municipio, municipio)) then
-		CriarMunicipio(est^.municipio, municipio)
-	else
-		writeln('Municipio já existe: ', municipio); //depuration, teste rs
 end;
 
-{procedure ExcluirMunicipio();
+procedure IncluirMunicipio();
+begin
+end;
+
+procedure ExcluirMunicipio();
 begin
 end;
 
 procedure cadastrarMunicipio();
 begin
-end;}
-
-procedure ProcessarOpcao();
-var
-	op: integer;
-	uf, municipio: string;
-begin
-	repeat
-		writeln('0 - FECHAR PROGRAMA ');
-		writeln('1 - INSERE ESTADO');
-		writeln('2 - INSERE MUNICIPIO');
-		writeln('3 - EXIBIR ARVORE EM ORDEM');
-		writeln('4 - EXIBIR ARVORE EM PRE-ORDEM');
-		write('Escolha uma opção: ');
-		readln(op);
-	
-		case op of
-			1: begin
-					writeln('Digite a sigla do estado a ser adicionado: ');
-					readln(uf);
-					CriarEstado(estados, uf);
-			   end;
-			 
-			2: begin
-					writeln('Digite a sigla do estado: ');                   
-					readln(uf);
-					writeln('Digite o nome do município: ' );
-					readln(municipio);
-					IncluirMunicipio(estados, uf, municipio);
-			   end;
-			 
-			3: begin
-					writeln('Exibindo árvore em ordem:');
-					EmOrdem(estados);
-					writeln;
-			   end;
-			 
-			4: begin
-					writeln('Exibindo árvore em pré-ordem:');
-					PreOrdem(estados);
-					writeln;
-			   end;
-			
-			0: writeln('Saindo do programa...');
-			
-			else
-				writeln('Opção inválida. Tente novamente.');
-		end;
-	until op = 0;
-end;
-
+end;  }
     
 Begin
-
-  IniciarVariaveis(estados);
-	ProcessarOpcao;
-	{IniciarVariaveis(estados);
+	IniciarVariaveis(estados);
   CriarEstado(estados, 'SC');
   CriarEstado(estados, 'SC');
   CriarEstado(estados, 'BB');
+  CriarEstado(estados, 'VV');
+  CriarEstado(estados, 'YY');
   CriarEstado(estados, 'ZZ');
+  CriarEstado(estados, 'TT');
+  CriarEstado(estados, 'RR');
+  CriarEstado(estados, 'SB');
+  CriarEstado(estados, 'SS');
+  CriarEstado(estados, 'UU');
+  CriarEstado(estados, 'XX');
+  
   writeln;
-  EmOrdem(estados); }
+  EmOrdem(estados);
+  writeln;
+  ExcluirEstado(estados, 'ZZ');
+  writeln;
+  EmOrdem(estados); 
 End.
